ccbb42f2713a619de0ce651e30dbc737fcabc1e1
bruno.fscosta <brunofsc_@hotmail.com>
2019-08-15 16:47:53 -0300

    mudar tamanho do food

---
+++ app/src/main/java/br/sp/senac/atividadedois/University_Activity.java
@@ -0,0 +1,14 @@
+package br.sp.senac.atividadedois;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class University_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.university_layout);
+    }
+}
